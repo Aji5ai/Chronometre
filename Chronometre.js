075b3d662671fc3adc15d99fd1f30068188e357b
Aji5ai <m1ck3ym0u5e@live.fr>
2023-03-30 11:45:31 +0200

    HTML updaté + variables js définies

---
+++ Chronometre.js
@@ -0,0 +1,15 @@
+// Définissons nos variables
+let chrono = document.getElementById("chrono");
+let startBtn = document.getElementById("start");
+let stopBtn = document.getElementById("stop");
+let resetBtn = document.getElementById("reset");
+
+let centisecondes = 0
+let secondes = 0
+let minutes = 0 
+let heures = 0
+
+let timeout;
+let estArrete = true;
+
+//
